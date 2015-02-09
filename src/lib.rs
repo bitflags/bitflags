bdb2a912ec0ebf97d756276a0f3a88cb50981cd5
Alex Crichton <alex@alexcrichton.com>
2015-02-09 09:22:09 -0800

    Remove #![no_std] for now
    This attribute is now feature gated.

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,7 @@
 
 //! A typesafe bitmask flag generator.
 
-#![feature(no_std)]
-
-#![no_std]
-#[cfg(test)] #[macro_use] extern crate std;
-
-extern crate core;
+#![cfg_attr(test, feature(hash))]
 
 /// The `bitflags!` macro generates a `struct` that holds a set of C-style
 /// bitmask flags. It is useful for creating typesafe wrappers for C APIs.
