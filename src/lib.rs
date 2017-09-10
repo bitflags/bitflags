612bac250afc4f12d5cab92c757e84f1096f8373
Tamir Duberstein <tamird@gmail.com>
2017-09-10 09:11:23 -0400

    Clarify purpose of libcore re-export

--- src/lib.rs
+++ src/lib.rs
@@ -225,8 +225,8 @@
 #[macro_use]
 extern crate std;
 
-// Re-export libstd/libcore using an alias so that the macros can work in no_std
-// crates while remaining compatible with normal crates.
+// Re-export libcore using an alias so that the macros can work without
+// requiring `extern crate core` downstream.
 #[doc(hidden)]
 pub extern crate core as _core;
 
