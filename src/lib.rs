5853685623ff72608c0a4b1a3eeff8089cb8101b
varkor <github@varkor.com>
2019-10-11 10:51:03 +0100

    Remove `#[inline]` on consts

--- src/lib.rs
+++ src/lib.rs
@@ -590,7 +590,6 @@ macro_rules! __impl_bitflags {
                     #[allow(non_snake_case)]
                     trait __BitFlags {
                         $(
-                            #[inline]
                             const $Flag: $T = 0;
                         )+
                     }
@@ -598,7 +597,6 @@ macro_rules! __impl_bitflags {
                         $(
                             __impl_bitflags! {
                                 #[allow(deprecated)]
-                                #[inline]
                                 $(? #[$attr $($args)*])*
                                 const $Flag: $T = Self::$Flag.bits;
                             }
