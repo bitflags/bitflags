71080a440ad32620dd5575dfd773efa1a9faa350
Nathaniel McCallum <npmccallum@redhat.com>
2019-12-16 21:46:47 +0000

    Modernize test_suite

--- test_suite/tests/external.rs
+++ test_suite/tests/external.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate bitflags;
+use bitflags::bitflags;
 
 bitflags! {
     /// baz
--- test_suite/tests/serde.rs
+++ test_suite/tests/serde.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate bitflags;
+use bitflags::bitflags;
 
 #[macro_use]
 extern crate serde_derive;
