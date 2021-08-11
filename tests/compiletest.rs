cf728d0257d9b0c26c57752d031e2b19efe5963a
Ashley Mannix <kodraus@hey.com>
2021-08-12 07:51:11 +1000

    comment the infra for stderr checking in beta

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -13,6 +13,14 @@ fn compile_fail() {
     t.compile_fail("tests/compile-fail/*.rs");
 }
 
+// Compiler messages may change between versions
+// We don't want to have to track these too closely for `bitflags`, but
+// having some message to check makes sure user-facing errors are sensical.
+// 
+// The approach we use is to run the test on all compilers, but only check stderr
+// output on beta (which is the next stable release). We do this by default ignoring
+// any `.stderr` files in the `compile-fail` directory, and copying `.stderr.beta` files
+// when we happen to be running on a beta compiler.
 fn prepare_stderr_files(path: impl AsRef<Path>) -> io::Result<()> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
@@ -20,6 +28,9 @@ fn prepare_stderr_files(path: impl AsRef<Path>) -> io::Result<()> {
         if entry.path().extension().and_then(OsStr::to_str) == Some("beta") {
             let renamed = entry.path().with_extension("");
 
+            // Unconditionally remove a corresponding `.stderr` file for a `.stderr.beta`
+            // file if it exists. On `beta` compilers, we'll recreate it. On other compilers,
+            // we don't want to end up checking it anyways.
             if renamed.exists() {
                 fs::remove_file(&renamed)?;
             }
