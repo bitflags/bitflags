db9b9f41413f4e6a2815d358d5c12723944a21bd
KodrAus <kodraus@hey.com>
2023-06-13 15:15:02 +1000

    prepare for 2.3.2 release

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-bitflags = "2.3.1"
+bitflags = "2.3.2"
 ```
 
 and this to your source code:
--- src/lib.rs
+++ src/lib.rs
@@ -422,7 +422,7 @@
 
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![cfg_attr(not(test), forbid(unsafe_code))]
-#![doc(html_root_url = "https://docs.rs/bitflags/2.3.1")]
+#![doc(html_root_url = "https://docs.rs/bitflags/2.3.2")]
 
 #[doc(inline)]
 pub use traits::{Flags, Flag, Bits};
