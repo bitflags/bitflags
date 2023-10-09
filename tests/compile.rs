dfe6f21e5947046553476be51973a9aaec6675bd
KodrAus <kodraus@hey.com>
2023-10-09 14:54:39 +1000

    just rely on smoke test for clippy

--- tests/compile.rs
+++ tests/compile.rs
@@ -11,13 +11,6 @@ fn fail() {
 
 #[test]
 fn pass() {
-    // If `clippy` is available, then use it
-    if std::process::Command::new("clippy-driver").arg("--version").output().is_ok() {
-        println!("Using `clippy`");
-
-        std::env::set_var("RUSTC_WORKSPACE_WRAPPER", "clippy-driver");
-    }
-
     let t = trybuild::TestCases::new();
     t.pass("tests/compile-pass/**/*.rs");
 }
